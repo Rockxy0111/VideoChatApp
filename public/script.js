0d5247cf0042ed313c0f926f742f54dd01b60f1b
RanveerKhokhar <72133434+Rockxy0111@users.noreply.github.com>
2023-12-06 16:36:08 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -106,7 +106,27 @@ $(function () {
             $("#stop_video").html(html)
         }
     })
+    $("#invite_button").click(function(){
+        const to = prompt("Enter your email address")
+        let data ={
+            url: window.location.href,
+            to: to
+        }
+        $.ajax({
+            url:"/send-mail",
+            type:"post",
+            data: JSON.stringify(data),
+            dataType:'json',
+            contentType:'application/json',
+            success: function (result){
+                alert("Invite sent!")
+            },
+            error: function (result) {
+                console.log(result.responseJSON)
+            }
 
+        })
+    })
 })
 
 peer.on("open", (id) => {
